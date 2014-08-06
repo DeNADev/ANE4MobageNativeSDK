019926aa750db0ab93ab4d0c6b9e465fe1c38a98
JoseLuis Takahashi <takahashi.joseluis@dena.jp>
2014-08-06 19:41:02 +0900

    - Added as default to add Google Play Games Services Library. See README-dev for more details.

--- common.mk
+++ common.mk
@@ -3,6 +3,10 @@
 # Path to Mobage Native SDK
 MOBAGE_SDK_PATH="Your_PATH_To_NATIVE_SDK/mobage-nativesdk-1.4.5-20131105-g6193da8"
 
+# Path for Google Play Games Services SDK and resources
+GOOGLE_SDK_PATH="Your_PATH_To_GooglePlay/google-play-services.jar"
+GOOGLE_SDK_RESOURCE_PATH="Your_PATH_To_GooglePlay_Res/google-play-services-res"
+
 # Path to FlashRuntimeExtension.jar bundled with Adobe Flash Builder
 FLASH_RUNTIME_EXT_JAR_PATH="/Applications/Adobe Flash Builder 4.7/sdks/4.6.0/lib/android/FlashRuntimeExtensions.jar"
 
